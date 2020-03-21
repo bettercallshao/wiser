aef8178895c9e33c6e251eca7d2af82028f7a040
Shaoqing Tan <bettercallshao@gmail.com>
2020-03-21 16:24:09 -0400

    Created with https://start.spring.io/ and validated on IJ

---
+++ src/main/java/com/bettercallshao/wiser/WiserApplication.java
@@ -0,0 +1,13 @@
+package com.bettercallshao.wiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiserApplication.class, args);
+	}
+
+}
